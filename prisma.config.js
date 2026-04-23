7790385ddd73806003406b63a12db7b5012778f7
ravinderdahiya <ravinderdh4@gmail.com>
2026-04-23 15:57:53 +0530

    update by ravinder

--- prisma.config.js
+++ prisma.config.js
@@ -1,7 +1,4 @@
 // prisma.config.js
 export default {
   schema: "./prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
 };
--- src/server.js
+++ src/server.js
@@ -1,18 +1,8 @@
-import express from "express"
-import dotenv from "dotenv"
-import userRoutes from "./user/user.routes.js"
+import dotenv from "dotenv";
 import app from "./app.js";
-import otpRoutes from "./otp/otp.routes.js"; 
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.use("/user", userRoutes)
-app.use("/otp", otpRoutes) // ✅ UNCOMMENT
 app.listen(8080, () => {
-  console.log("Server running on 8080")
-})
+  console.log("Server running on 8080");
+});
